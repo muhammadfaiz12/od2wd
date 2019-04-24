57368df0e90cd32dcd40cc7fdb89617d326df677
muhammad.faiz <faiz00.muhammad@gmail.com>
2019-04-24 11:58:32 +0700

    change routing button

--- var_settings.py
+++ var_settings.py
@@ -2,6 +2,8 @@ def init_global_var():
     global protagonist_dict
     global entityheader_dict
     global mapping_dict
+    global parent_link
     protagonist_dict={}
     entityheader_dict={}
     mapping_dict={}
+    parent_link="http://localhost:5000"
