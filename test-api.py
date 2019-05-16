74451545ee0f41f14e3ec9c154371e16601ddfb8
muhammad.faiz <faiz00.muhammad@gmail.com>
2019-05-16 23:53:44 +0700

    test api

--- test-api.py
+++ test-api.py
@@ -1,1 +1,40 @@
-def test_api(column, dttype):
+import json
+import requests
+
+def test_api(columns, dttype):
+    properties = []
+    parent_api_link="http://od2wd.id/api/"
+    for col in columns:
+        obj = {}
+        obj['item'] = col
+        obj['item_range'] = dttype[col]
+        obj['limit'] = 5
+        properties.append(obj)
+    payload = {}
+    payload['properties']=properties
+    # payload = json.dumps(payload)
+    print(payload)
+    print("PHASE-2, Calling Url for Property Mapping")
+    url = "{}/main/property".format(parent_api_link)
+    response = requests.post(url, json=payload)
+    
+    json_data = json.loads(response.text)
+    result = {}
+    result_label = {}
+    print(json_data)
+    for obj in json_data['results']:
+        if len(obj['map_to']) > 0:
+            result[str(obj['item'])] = obj['map_to'][0]['id']
+            result_label[str(obj['item'])] = obj['map_to'][0]['label']
+        else:
+            result[str(obj['item'])] = ''
+            result_label[str(obj['item'])] = ''
+    return result, result_label
+
+
+column=['alamat', 'kelurahan', 'alamat sekolah']
+dt_col={'alamat':'String', 'kelurahan':'WikibaseItem', 'alamat sekolah':'WikibaseItem'}
+a, b = test_api(column, dt_col)
+print(a)
+print(b)
+    
