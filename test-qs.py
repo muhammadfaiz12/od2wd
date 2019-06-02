b36d7e3ed7c5a90ab1452a6f5137e13e38de77af
muhammad.faiz <saravedian@gmail.com>
2019-06-02 23:38:55 +0700

    adding modification in test-qs

--- test-qs.py
+++ test-qs.py
@@ -38,15 +38,16 @@ def test_publish_qs_direct(procId):
     url="https://tools.wmflabs.org/quickstatements/api.php"
     payload = {}
     payload['action'] = 'import'
-    payload['format'] = 'csv'
     payload['submit'] = 1
-    # payload['openpage'] = 1
-    # payload['temporary'] = 1
-    payload['data'] = csv
     payload['username'] = 'od2wd'
     payload['token'] = "%242y%2410%24rI5bLyGIWFKgFGeISauRHOeS1S7un5iwlqBDfcWmKHp9LEGqcUKTG"
+    payload['format'] = 'csv'
+    payload['data'] = csv
+    payload['batchname'] = 'od2wd-wikidata-integration'
+    # payload['openpage'] = 1
+    # payload['temporary'] = 1
     payload_str = "&".join("{}={}".format(k,v) for k,v in payload.items())
-    response = requests.get(url, params=payload_str)
+    response = requests.post(url, params=payload_str)
     print(response.url)
     print(response.text)
 
