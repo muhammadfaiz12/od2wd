3e9e77dbf58b4cc3552810dda00582f736ebcbb8
Muhammad Faiz <muhammad.faiz@ID-MUHAMMADFAIZ.local>
2020-07-13 22:36:32 +0700

    feat-inputmetadata-add global dict

--- var_settings.py
+++ var_settings.py
@@ -5,10 +5,13 @@ def init_global_var():
     global parent_link
     global mappingbeautified_dict
     global parent_api_link
+    global job_metadata_dict
     protagonist_dict={}
     entityheader_dict={}
     mapping_dict={}
     mappingbeautified_dict={}
+    # Job_metdata_dict [jobId:str -> {key:str -> value:str}]
+    job_metadata_dict={}
     #Production
     parent_link="http://od2wd.id"
     parent_api_link="http://localhost:8080/"
